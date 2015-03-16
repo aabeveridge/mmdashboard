383dd223a2391b2942bd83e92eac2ab4c62e20b4
Aaron Beveridge <aaronbev79@gmail.com>
2015-03-16 15:46:57 -0400

    most recent updates to app

--- server.R
+++ server.R
@@ -6,15 +6,29 @@ library(markdown)
 
 shinyServer(function(input, output, session) {
     
+  #This is the output for the summary tab
+  #The input for this tab are mapped from the table1 and file1 inputs
+  #from the UI page
   output$table1 <- renderTable({
+    
+    #The withProgress() function lets the user know that the
+    #system is working. There are ways to make it more
+    #complex by embedding the function in an if statement
+    #while 
     withProgress(message="PLEASE WAIT...", {
       incProgress(0.3, detail="working")
       
+      #Loading the file into shiny with the browse function
       infile <- input$file1
       if (is.null(infile)) {
         return(NULL)
       }
+      
+      #The "datapath" is where the input$file1 actually saves
+      #the loaded file in memory/temp
       load(infile$datapath)
+      
+      #This creates the word frequency table on summary page
       freq <- colSums(as.matrix(dtm))
       ord <- order(freq)
       freq1 <- as.data.frame(freq[tail(ord, input$length1)])
@@ -23,6 +37,7 @@ shinyServer(function(input, output, session) {
     })
   })
   
+  #This doesn't work yet, and I don't remember what I was making...lol
   output$table2 <- renderTable({
       withProgress(message="PLEASE WAIT...", {
         incProgress(0.3, detail="working")
@@ -36,6 +51,12 @@ shinyServer(function(input, output, session) {
         length(freq)
       })
   })
+  
+  #This is the output for the Time Series tab in the UI
+  #You can change this to renderPlot or other reactive
+  #functionality depending on your output
+  output$table3 <- renderTable({
+  }) 
   
   #Create cluster graph for Cluster Analysis tab      
   output$plot <- renderPlot({
--- ui.R
+++ ui.R
@@ -5,12 +5,22 @@ library(stringr)
 library(markdown)
 
 shinyUI(navbarPage("MassMine",
+                   
+                    #Main page
                     tabPanel("Welcome",
                              mainPanel(
                                       includeMarkdown("./other_files/filepage.md"))),
+                    
+                    #navbarMenu() function allows a tab at the top of the page
+                    #to become a drop down menut
                     navbarMenu("Topic",
+                  
+                      #Each tabPanel() is an item on the navbarMenu()
                       tabPanel("Summary", 
-                            sidebarPanel("Additional Adjustments",
+                            
+                              #These are the inputs for the summary page--their actual
+                              #variables are the first word in quotes inside the function
+                              sidebarPanel("Additional Adjustments",
                                          fileInput("file1", "Select File for Analysis",
                                                    multiple=FALSE, accept = c(".rda"
                                                    )),                               
@@ -21,26 +31,51 @@ shinyUI(navbarPage("MassMine",
                                                      "Display Most Frequent Words",
                                                      min = 10, max = 100, value = 10)
                             ),
+                            
+                            #This is the main output page for the Summary tab
                             mainPanel(
-#                               column(4,
                                 tableOutput("table1")
-#                               column(4,
-#                                 tableOutput("table2"))
                             )
                       ),
+                      
+                      #Another item in the drop down menu--this is empty so far
                       tabPanel("Frequency",
                               dataTableOutput("table2")
                       ),
+                      
+                      #This one is for your time series analysis
+                      tabPanel("Time Series",
+                              dataTableOutput("table3")
+                      ),
+                      
+                      #The cluster analysis
                       tabPanel("Cluster",
                              
-                            wellPanel(
-                               sliderInput("words",
-                                           "Number of Words in Cluster",
-                                           min = 10, max = 100, value = 50)
-                             ),                           
-                            plotOutput("plot", width="auto", height="600px")
-                             
-                        )),
+                               #This tabsetPanel() function allows there to be separate tabs on a
+                               #single selected page within the drop down menue
+                               tabsetPanel(type = "tabs", 
+                                           
+                                           #First tab for this page
+                                           tabPanel("Cluster",
+                                              
+                                              #The wellPanel() function allows the controls to be
+                                              #vertically distributed in the same page--this allows
+                                              #the cluster analysis to fill the full page width below
+                                              wellPanel(
+                                                sliderInput("words",
+                                                            "Number of Words in Cluster",
+                                                            min = 10, max = 100, value = 50)
+                                                
+                                              ),
+                                              plotOutput("plot", width="auto", height="600px")
+                                           ),
+                                           
+                                           #Second tab for this page. I know what I want to do with
+                                           #this page, but I haven't filled it in yet
+                                           tabPanel("Word Correlations", plotOutput("plot.cor"))
+                    ))),
+                    
+                    #This is location analysis--similar to what we are doing with our Twitter publication
                     navbarMenu("Location",
                               tabPanel("Data"),
                               tabPanel("Summary"),

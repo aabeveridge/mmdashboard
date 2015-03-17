e9da6f6a93e90b75672f8552a7d0e35ef2ceba14
Aaron Beveridge <aaronbev79@gmail.com>
2015-03-17 18:12:10 -0400

    new updates

--- server.R
+++ server.R
@@ -9,7 +9,7 @@ shinyServer(function(input, output, session) {
   #This is the output for the summary tab
   #The input for this tab are mapped from the table1 and file1 inputs
   #from the UI page
-  output$table1 <- renderTable({
+  output$freqtable <- renderTable({
     
     #The withProgress() function lets the user know that the
     #system is working. There are ways to make it more
@@ -19,7 +19,7 @@ shinyServer(function(input, output, session) {
       incProgress(0.3, detail="working")
       
       #Loading the file into shiny with the browse function
-      infile <- input$file1
+      infile <- input$filerda
       if (is.null(infile)) {
         return(NULL)
       }
@@ -31,40 +31,52 @@ shinyServer(function(input, output, session) {
       #This creates the word frequency table on summary page
       freq <- colSums(as.matrix(dtm))
       ord <- order(freq)
-      freq1 <- as.data.frame(freq[tail(ord, input$length1)])
+      freq1 <- as.data.frame(freq[tail(ord, input$freqsl)])
       names(freq1) <- c("Total")
       freq1
     })
   })
   
   #This doesn't work yet, and I don't remember what I was making...lol
-  output$table2 <- renderTable({
-      withProgress(message="PLEASE WAIT...", {
-        incProgress(0.3, detail="working")
-      
-        infile <- input$file1
-        if (is.null(infile)) {
-          return(NULL)
-        }
-        load(infile$datapath)
-        freq <- colSums(as.matrix(dtm))
-        length(freq)
-      })
-  })
+#   output$table.hash <- renderTable({
+#     withProgress(message="PLEASE WAIT...", {
+#       incProgress(0.3, detail="working")
+#       infile <- input$filerda
+#       if (is.null(infile)) {
+#         return(NULL)
+#       }
+#       load(infile$datapath)
+#       
+#       ## Let's dig the hashtags out of the dtm
+#       
+#       head(d$text)
+#       ## This retrieves the indices of hashtags
+#       raw_hashtags = unlist(str_extract_all(d$text,
+#                                             "#[[:alpha:]][[:alnum:]_]+ "))
+#       ## Number of occurences for each hashtag
+#       hashtags = as.data.frame(sort(table(raw_hashtags),
+#                                  decreasing=TRUE))
+#       names(hashtags) = c("Total")
+#       
+#       ## Display result
+#       hashtags
+#       
+#     })
+#   }) 
   
   #This is the output for the Time Series tab in the UI
   #You can change this to renderPlot or other reactive
   #functionality depending on your output
-  output$table3 <- renderTable({
-  }) 
+#   output$series <- renderTable({
+#   }) 
   
   #Create cluster graph for Cluster Analysis tab      
-  output$plot <- renderPlot({
+  output$clust <- renderPlot({
     
     #Call a list of attributions to change the look of the cluster graph
     defAttrs <- getDefaultAttrs()
     
-    infile <- input$file1
+    infile <- input$filerda
     if (is.null(infile)) {
       return(NULL)
     }
@@ -75,12 +87,31 @@ shinyServer(function(input, output, session) {
       incProgress(0.3, detail="working")
       
       #Creates cluster analysis using Rgraphviz
-      plot(dtm, terms=findFreqTerms(dtm, lowfreq=20)[1:input$words], 
+      plot(dtm, terms=findFreqTerms(dtm, lowfreq=20)[1:input$clust.sl], 
            corThreshold=0.0, attrs=list(node=list(shape = "ellipse", fixedsize = FALSE, 
                                                   fillcolor="lightblue", height="2.6", width="10.5", 
                                                   fontsize="14")))
       #Increment progress 100%
       incProgress(1, detail="complete")
+    })
+  })
+
+  output$corr <- renderTable({
+    withProgress(message="PLEASE WAIT...", {
+      incProgress(0.3, detail="working")
+      
+      #Loading the file into shiny with the browse function
+      infile <- input$filerda
+      if (is.null(infile)) {
+        return(NULL)
+      }
+      
+      #The "datapath" is where the input$file1 actually saves
+      #the loaded file in memory/temp
+      load(infile$datapath)
+      
+      d <- findAssocs(dtm, input$text, corlimit=0.0)
+      head(d, input$corr.sl)
     })
   })
 })
--- ui.R
+++ ui.R
@@ -7,84 +7,64 @@ library(markdown)
 shinyUI(navbarPage("MassMine",
                    
                     #Main page
-                    tabPanel("Welcome",
+                    tabPanel("Load Data",
+                             sidebarPanel(
+                                          fileInput("filerda", "Load File",
+                                                    multiple=FALSE, accept = c(".rda"
+                                                    ))),
                              mainPanel(
                                       includeMarkdown("./other_files/filepage.md"))),
                     
-                    #navbarMenu() function allows a tab at the top of the page
-                    #to become a drop down menut
-                    navbarMenu("Topic",
                   
-                      #Each tabPanel() is an item on the navbarMenu()
-                      tabPanel("Summary", 
+                    #Summary panel
+                    tabPanel("Summary", 
                             
-                              #These are the inputs for the summary page--their actual
-                              #variables are the first word in quotes inside the function
-                              sidebarPanel("Additional Adjustments",
-                                         fileInput("file1", "Select File for Analysis",
-                                                   multiple=FALSE, accept = c(".rda"
-                                                   )),                               
-                                         sliderInput("sparse", 
-                                                     "Remove Sparse Words",
-                                                     min = 0.01, max = .1, value = 0.05, step=.01),
-                                         sliderInput("length1",
+                            #These are the inputs for the summary page--their actual
+                            #variables are the first word in quotes inside the function
+                            sidebarPanel(
+                                         sliderInput("freqsl",
                                                      "Display Most Frequent Words",
                                                      min = 10, max = 100, value = 10)
                             ),
                             
                             #This is the main output page for the Summary tab
                             mainPanel(
-                                tableOutput("table1")
+                                tableOutput("freqtable") #,
+#                                 tableOutput("hash"),
+#                                 tableOutput("user")
                             )
                       ),
                       
-                      #Another item in the drop down menu--this is empty so far
-                      tabPanel("Frequency",
-                              dataTableOutput("table2")
-                      ),
-                      
                       #This one is for your time series analysis
-                      tabPanel("Time Series",
-                              dataTableOutput("table3")
-                      ),
+#                       tabPanel("Time Series",
+#                               dataTableOutput("")
+#                       ),
                       
                       #The cluster analysis
                       tabPanel("Cluster",
-                             
-                               #This tabsetPanel() function allows there to be separate tabs on a
-                               #single selected page within the drop down menue
-                               tabsetPanel(type = "tabs", 
-                                           
-                                           #First tab for this page
-                                           tabPanel("Cluster",
                                               
-                                              #The wellPanel() function allows the controls to be
-                                              #vertically distributed in the same page--this allows
-                                              #the cluster analysis to fill the full page width below
-                                              wellPanel(
-                                                sliderInput("words",
-                                                            "Number of Words in Cluster",
-                                                            min = 10, max = 100, value = 50)
+                              #The wellPanel() function allows the controls to be
+                              #vertically distributed in the same page--this allows
+                              #the cluster analysis to fill the full page width below
+                              wellPanel(
+                                        sliderInput("clust.sl",
+                                                    "Number of Words in Cluster",
+                                                    min = 10, max = 100, value = 50)
                                                 
-                                              ),
-                                              plotOutput("plot", width="auto", height="600px")
-                                           ),
+                                                    ),
+                                        plotOutput("clust", width="auto", height="600px")
+                                        ),
                                            
-                                           #Second tab for this page. I know what I want to do with
-                                           #this page, but I haven't filled it in yet
-                                           tabPanel("Word Correlations", plotOutput("plot.cor"))
-                    ))),
-                    
-                    #This is location analysis--similar to what we are doing with our Twitter publication
-                    navbarMenu("Location",
-                              tabPanel("Data"),
-                              tabPanel("Summary"),
-                              tabPanel("Map"),
-                              tabPanel("Analysis")),
-                               
-                    tabPanel("Help",
-                          mainPanel(
-                              includeMarkdown("./other_files/help.md")))
-)
+                      #Second tab for this page. I know what I want to do with
+                      #this page, but I haven't filled it in yet
+                      tabPanel("Correlation",
+                               sidebarPanel(
+                                            sliderInput("corr.sl", "Length of List",
+                                                        min=10, max=100, value=20),
+                                            textInput("text", "Text:", "text here")),
+                               mainPanel("Word Correlations",
+                               tableOutput("corr"))
+                      )
+                    )
 
         )
